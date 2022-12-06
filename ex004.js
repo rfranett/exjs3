fbde222330bc66115a4c56efb2ed0c61a0da13c9
rfranett <rfranett@gmail.com>
2022-12-06 13:59:52 -0300

    adicionando mais um card

--- ex004.js
+++ ex004.js
@@ -3,6 +3,11 @@ let :jogo ={
     desenvolvedor : 'Riot Games',
     anoLancamento :2009
 }
+let :jogo2 ={
+    titulo :'Blankos Brock party',
+    desenvolvedor : 'mythical Games',
+    anoLancamento :2020
+}
 
 document.write(`<div class="jogo">
 <h2>Jogo</h2>
@@ -12,5 +17,16 @@ document.write(`<div class="jogo">
 <p>${jogo.desenvolvedor}</p>
 <span>Ano do Lançamento</span>
 <p>${jogo.anoLancamento}</p>
+</div>
+`)
+
+document.write(`<div class="jogo2">
+<h2>Jogo</h2>
+<span>Tìtulo</span>
+<p>${jogo2.titulo}</p>
+<span>Desenvolvedor</span>
+<p>${jogo2.desenvolvedor}</p>
+<span>Ano do Lançamento</span>
+<p>${jogo2.anoLancamento}</p>
 </div>
 `)
